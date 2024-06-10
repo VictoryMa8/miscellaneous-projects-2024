42821581cae0d3587155eec2af83eecf6a7bee43
Victor <160278295+VictoryMa8@users.noreply.github.com>
2024-06-09 21:31:18 -0500

    Movie database with Firebase

---
+++ movieDatabase/index.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-ca1ee-default-rtdb.firebaseio.com/"
+}
+
+// retrieves our realtime database that we have on Firebase via the URL and the functions from Firebase
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDB = ref(database, "movies")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(moviesInDB, inputValue)
+    
+    console.log(`${inputValue} added to database`)
+})
